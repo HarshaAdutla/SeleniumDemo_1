99bd3b94ff1816812e92853a2338ba0ff440a9f0
HarshaAdutla <89002868+HarshaAdutla@users.noreply.github.com>
2023-03-29 11:06:46 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,4 @@
-/* import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-
-public class Main {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "chromedriver");
-        WebDriver driver = new ChromeDriver();
-//        try {
-//            Thread.sleep(500);
-//        } catch (InterruptedException e) {
-//            System.out.println(e);
-//        }
-        driver.get("www.projects.awigntest.com");
-        driver.manage().window().maximize();
-        driver.close();
-    }
-}
-*/
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,22 +10,20 @@ public class Main {
         ChromeDriverService service = ChromeDriverService.createDefaultService();
         WebDriver driver = new ChromeDriver(service);
         driver.manage().window().maximize();
-        driver.get("www.awigntest.com");
-                Thread.sleep(2000);
-        driver.findElement(By.className("pointer ")).click();
+        driver.get("www.projects.awigntest.com");
+        Thread.sleep(2000);
+        driver.findElement(By.className("pointer ")).click(); // Here we are clicking on Sign in CTA
         Thread.sleep(3000);
-        driver.findElement(By.xpath("//input[@placeholder = 'Email']")).sendKeys("hr050798@gmail.com");
+        driver.findElement(By.xpath("//input[@placeholder = 'Email']")).sendKeys("hr050798@gmail.com"); // Passing the email
         Thread.sleep(1000);
-        driver.findElement(By.xpath("//input[@placeholder = 'Password']")).sendKeys("050798");
+        driver.findElement(By.xpath("//input[@placeholder = 'Password']")).sendKeys("050798"); // Passing the Password
         Thread.sleep(1000);
-        driver.findElement(By.className("btn-primary")).click();
+        driver.findElement(By.className("btn-primary")).click(); // Clicking on Sign in CTA after entering the email and password
         Thread.sleep(4000);
-//        String Error_message = driver.findElement(By.className("text-danger")).getText();
-//        assert Error_message.contains("Invalid");
-//        System.out.println("Entered credentials are wrong, please check and try again");
         System.out.println("Successfully Logged into Projects Platform");
-        driver.findElement(By.xpath("//button[@type= 'button']")).click();
+        driver.findElement(By.xpath("//button[@type= 'button']")).click(); // Here we are clicking on All Projects option
         Thread.sleep(6000);
-        System.out.println(driver.getTitle());
+        System.out.println(driver.getTitle()); // It will fetch the wesite titile
+        driver.close();
     }
 }
